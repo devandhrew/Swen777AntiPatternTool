bbb601a01e121ef88a241db0d8d861a3c235cbe4
Stephen Cook <sjc5897@rit.edu>
2021-05-09 18:36:34 -0400

    B.2 created methods

--- src/test/validation.java
+++ src/test/validation.java
@@ -3,6 +3,70 @@ package src.test;
 public class validation {
     //10 invalid methods
 
+
+    //Created method edge cases
+    //6
+    //Created method, detects all capitals
+    public int CHECK_Valid(){
+        return 1;
+    }
+
+    //7
+    //created method, simple name
+    public int check(){
+        return 1;
+    }
+
+    //8
+    //created method, underscore
+    public int validate_number(){
+        return 1;
+    }
+
+    //9
+    //created method, hungarian notation
+    public int iEnsureValid(){
+        return 1;
+    }
+
+    //10
+    //method created, word twice in function
+    public int validateChecker(){
+        return 1;
+    }
+
     //10 valid methods
+
+    //Created method edge cases
+    //6
+    //created method, starts with word
+    public int checkersCount(){
+        return 1;
+    }
+
+    //7
+    //created method, underscore
+    public boolean check_valid(){
+        return true;
+    }
+
+    //8
+    //created method, hungarian notation
+    public Boolean bCheckValid(){
+        return true;
+    }
+
+    //9
+    //created method, does not start, but contains string
+    public int countCheckers(){
+        return 1;}
+
+    //10
+    //method created, word twice in function
+    public boolean checkCheckers() {
+        return true;
+    }
+
+
 
 }
